99e1a25d5cc88ac5692caa27c9affe1bf799317e
Karim Ahmed <karim.ahmed14770@gmail.com>
2026-02-03 18:51:25 +0200

    feat: implement logic and dependency injection

--- src/com/KarimAhmed/TaskManager/Service/TaskService.java
+++ src/com/KarimAhmed/TaskManager/Service/TaskService.java
@@ -1,12 +1,6 @@
 package com.KarimAhmed.TaskManager.Service;
 
 
-/*
-* Think of the TaskService as the "Engine Room." It doesn't care about Scanner
-* or System.out.print (that belongs in Main). It only cares about manipulating the data.
-*
-* */
-
 
 
 /*importing packages*/
@@ -16,36 +10,101 @@ import com.KarimAhmed.TaskManager.Model.TaskStatus;
 import com.KarimAhmed.TaskManager.Repository.TaskRepository;
 
 import java.util.List;
-import java.util.ArrayList;
 
 
 public class TaskService {
     private List<Task> tasksList;//List interface which would give me freedom
     //to either implement it as ArrayList or as LinkedList
-    private int nextID=1;
+    private int nextId;
     //the Unique ID for each Task
     private TaskRepository repository;
     //the repository object responsible for handling storage data
 
 
-    public TaskService(TaskRepository repository){
-        tasksList=new ArrayList<>();
-        this.repository=repository;
-        nextID=1;//function for calculating id;
+    /*dependency injection */
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+        tasksList = repository.loadTasks();
+        nextId = calculateId(tasksList);//function for calculating id;
     }
 
     /*helper Methods*/
-    private int calculateID(){
 
-        return 0;
+    /*calculate the id of the next task, ensuring each task having a unique ID*/
+    private int calculateId(List<Task> tasksList) {
+        int next;
+        if (tasksList.isEmpty()) {
+            next = 1;
+        } else {
+            next = tasksList.getFirst().getId();
+            for (Task x : tasksList) {
+                if (x.getId() >= next) {
+                    next = x.getId();
+                }
+            }
+            next++;
+        }
+        return next;
+    }
+
+    /*returns the index of the task if found, if not it returns -1*/
+    private int searchByID(int id) {
+        int result = -1;
+        for (int i = 0; i < tasksList.size(); i++) {
+            if (id == tasksList.get(i).getId()) {
+                result = i;
+                break;
+            }
+        }
+        return result;
     }
 
     /*public methods*/
-    public void addTask(String name,String description){
-        Task x=new Task(name,description);
-        x.setId(calculateID());
+    public void addTask(String name, String description) {
+        Task x = new Task(name, description);
+        x.setId(nextId);
+        nextId++;
         tasksList.add(x);
+        repository.saveTasks(tasksList);
     }
 
+    public void displayTasks() {
+        for (Task x : tasksList) {
+            System.out.println(x);
+        }
+    }
 
+    public void updateTaskStatus(int id, TaskStatus status) {
+        if (id > 0 && id < nextId) {
+            int index = searchByID(id);
+            if (index != -1) {
+                tasksList.get(index).setStatus(status);
+                repository.saveTasks(tasksList);
+            } else {
+                //index with that id is not found
+                System.out.println("Task wit ID: "+id+" is not found");
+            }
+        }
+        else{
+            System.out.println("Invalid ID");
+        }
+    }
+
+    public void deleteTask(int id) {
+        if (id > 0 && id < nextId) {
+            int index = searchByID(id);
+            if (index != -1) {
+                tasksList.remove(index);
+                repository.saveTasks(tasksList);
+            } else {
+                //index with thatid is not found
+                System.out.println("Task wit ID: "+id+" is not found");
+            }
+        }
+        else{
+            System.out.println("Invalid ID");
+        }
+
+
+    }
 }
